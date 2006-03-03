6e99aea2ad41c6a89ea53d7c3a8c72767008bfb6
squeegy <squeegy@3ad9c75a-930d-0410-97f1-af11573dca1c>
2006-03-03 18:38:40 +0000

    added a migration generator
    git-svn-id: http://beautifulpixel.com/svn/plugins/settings@3 3ad9c75a-930d-0410-97f1-af11573dca1c

---
+++ generators/settings_migration/settings_migration_generator.rb
@@ -0,0 +1,12 @@
+class SettingsMigrationGenerator < Rails::Generator::NamedBase
+  def initialize(runtime_args, runtime_options = {})
+    runtime_args << 'add_settings_table' if runtime_args.empty?
+    super
+  end
+
+  def manifest
+    record do |m|
+      m.migration_template 'migration.rb', 'db/migrate'
+    end
+  end
+end
---
+++ generators/settings_migration/templates/migration.rb
@@ -0,0 +1,14 @@
+class <%= class_name %> < ActiveRecord::Migration
+  def self.up
+    create_table :settings, :force => true do |t|
+        t.column :var, :string, :null => false
+        t.column :value, :string, :null => true
+        t.column :created_at, :datetime
+        t.column :updated_at, :datetime
+    end
+  end
+
+  def self.down
+    drop_table :settings
+  end
+end
