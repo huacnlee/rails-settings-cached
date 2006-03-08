2a55760882c46cf813facd95ce8bc2d605c839cc
squeegy <squeegy@3ad9c75a-930d-0410-97f1-af11573dca1c>
2006-03-08 08:00:16 +0000

    added support for a cache to prevent duplicate DB queries
    git-svn-id: http://beautifulpixel.com/svn/plugins/settings@4 3ad9c75a-930d-0410-97f1-af11573dca1c

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,72 +1,64 @@
 class Settings < ActiveRecord::Base
-	
-	def self.method_missing(method, *args)
-		
-		if var = find(:first, :conditions => ['var = ?', method.to_s])
-			#varible exists, retrieve value
-			
-			var.value
-			
-		elsif method.to_s.include? '='
-			#set a value for a variable
-			
-			var_name = method.to_s.gsub('=', '')
-			value = args.first
-			
-			if var = find(:first, :conditions => ['var = ?', var_name])
-				#var exists, set new value
-				var.value = value
-				var.save
-				
-			else
-				#var does not exists, create new record
-				
-				var = self.create(
-					:var => var_name,
-					:value => value
-				)
-			end
-			
-			var.value
-		else
-			raise "Setting variable \"#{method.to_s}\" not found"
-		end
-	end
-	
-	def self.destroy(var_name)
-		if var = find(:first, :conditions => ['var = ?', var_name.to_s])
-			#varible exists, destroy row
-			var.destroy
-		else
-			raise "Setting variable \"#{var_name}\" not found"
-		end
-	end
-	
-	def self.all
-		#retrieve all settings as a hash
-		vars = find(:all)
-		
-		result = {}
-		vars.each do |record|
-			result[record.var] = record.value
-		end
-		result.with_indifferent_access
-	end
+  @cache = {}
+
+  def self.method_missing(method, *args)
+    #get or set a variable with the variable as the called method
+    method_name = method.to_s
+    
+    if method_name.include? '='
+      #set a value for a variable
+      var_name = method_name.gsub('=', '')
+      value = args.first
+      self[var_name] = value
+    else
+      #retrieve a value
+      self[method_name]
+    end
+  end
+
+  def self.destroy(var_name)
+    return @cache.delete(var_name.to_s) if delete_all(['var = ?', var_name.to_s]) #variable exists, destroy row and cache
+    raise "Setting variable \"#{var_name}\" not found"
+  end
+
+  def self.all
+    #retrieve all settings as a hash
+    vars = find(:all, :select => 'var, value')
+    
+    result = {}
+    vars.each do |record|
+      result[record.var] = record.value
+    end
+    @cache = result
+    result.with_indifferent_access
+  end
+  
+  def self.reload
+    #reload all settings form the db
+    self.all
+    self
+  end
+
+  def self.[](var_name)
+    #retrieve a setting
+    var_name = var_name.to_s
+    
+    return @cache[var_name] if @cache[var_name] #return cached value
+    
+    if var = find(:first, :conditions => ['var = ?', var_name])
+      @cache[var_name] = var.value
+      var.value
+    else
+      nil
+    end
+  end
+
+  def self.[]=(var_name, value)
+    #set a value to a var name
+    var_name = var_name.to_s
+    
+    if (update_all(['value = ?',value], ['var = ?',var_name]) > 0) || create(:var => var_name, :value => value)
+      @cache[var_name] = value 
+    end
+  end
 end
-
-
-##Settings table migration:
-#class CreateSettingsTable < ActiveRecord::Migration
-#	def self.up
-#		create_table :settings do |t|
-#			t.column :var, :string, :null => false
-#			t.column :value, :string, :null => true
-#			t.column :created_at, :datetime
-#			t.column :updated_at, :datetime
-#		end
-#	end
-#
-#	def self.down
-#		drop_table :settings
-#	end
-#end
--- test/settings_test.rb
+++ test/settings_test.rb
@@ -10,16 +10,27 @@ class SettingsTest < Test::Unit::TestCase
 	
 	def test_get
 		assert_equal 'foo', Settings.test
+		assert_equal 'foo', Settings[:test]
+		assert_equal 'foo', Settings['test']
+		
 		assert_equal 'bar', Settings.secondary_test
+		assert_equal 'bar', Settings[:secondary_test]
+		assert_equal 'bar', Settings['secondary_test']
 	end
 	
 	def test_set
 		assert_equal '321', Settings.test = '321'
+		assert_equal '321', Settings[:test] = '321'
+		assert_equal '321', Settings['test'] = '321'
+		
 		assert_equal '321', Settings.test
 	end
 	
 	def test_create
 		assert_equal '123', Settings.onetwothree = '123'
+		assert_equal '123', Settings[:onetwothree] = '123'
+		assert_equal '123', Settings['onetwothree'] = '123'
+		
 		assert_equal '123', Settings.onetwothree
 	end
 end
