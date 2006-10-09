cd8a2fa2a52d7c711f3342490cfae207d03baf96
squeegy <squeegy@3ad9c75a-930d-0410-97f1-af11573dca1c>
2006-10-09 23:46:42 +0000

    [settings] Added a Settings.object(var_name) method that allows you to easily get the actual AR object for any setting.  The getter and setter for the "value" attribute automatically encode and decode YAML for DB storage.
    git-svn-id: http://beautifulpixel.com/svn/plugins/settings@80 3ad9c75a-930d-0410-97f1-af11573dca1c

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,13 +1,15 @@
 class Settings < ActiveRecord::Base
-  @@defaults      = (defined?(SettingsDefaults) ? SettingsDefaults::DEFAULTS : {}).with_indifferent_access
+  @@defaults  = (defined?(SettingsDefaults) ? SettingsDefaults::DEFAULTS : {}).with_indifferent_access
   
   class SettingNotFound < RuntimeError; end
   
   #get or set a variable with the variable as the called method
   def self.method_missing(method, *args)
     method_name = method.to_s
+    super(method, *args)
     
-    if method_name.include? '='
+  rescue NoMethodError
+    if method_name[-1..-1] == '='
       #set a value for a variable
       var_name = method_name.gsub('=', '')
       value = args.first
@@ -22,7 +24,7 @@ class Settings < ActiveRecord::Base
   def self.destroy(var_name)
     var_name = var_name.to_s
     if self[var_name]
-      find(:first, :conditions => ['var = ?', var_name]).destroy
+      object(var_name).destroy
       true
     else
       raise SettingNotFound, "Setting variable \"#{var_name}\" not found"
@@ -35,23 +37,18 @@ class Settings < ActiveRecord::Base
     
     result = {}
     vars.each do |record|
-      result[record.var] = YAML::load(record.value)
+      result[record.var] = record.value
     end
     result.with_indifferent_access
   end
   
-  #reload all settings from the db
-  def self.reload # :nodoc:
-    self # deprecated, no longer needed since caching is not used.
-  end
-  
-  #retrieve a setting value bar [] notation
+  #retrieve a setting value by [] notation
   def self.[](var_name)
     #retrieve a setting
     var_name = var_name.to_s
     
-    if var = find(:first, :conditions => ['var = ?', var_name])
-      YAML::load(var.value)
+    if var = object(var_name)
+      var.value
     elsif @@defaults[var_name]
       @@defaults[var_name]
     else
@@ -61,12 +58,30 @@ class Settings < ActiveRecord::Base
   
   #set a setting value by [] notation
   def self.[]=(var_name, value)
-    if self[var_name] != value
-      var_name = var_name.to_s
-      
-      record = Settings.find(:first, :conditions => ['var = ?', var_name]) || Settings.new(:var => var_name)
-      record.value = value.to_yaml
-      record.save
-    end
+    var_name = var_name.to_s
+    
+    record = object(var_name) || Settings.new(:var => var_name)
+    record.value = value
+    record.save
+  end
+  
+  #retrieve the actual Setting record
+  def self.object(var_name)
+    Settings.find_by_var(var_name.to_s)
+  end
+  
+  #get the value field, YAML decoded
+  def value
+    YAML::load(self[:value])
+  end
+  
+  #set the value field, YAML encoded
+  def value=(new_value)
+    self[:value] = new_value.to_yaml
+  end
+  
+  #Deprecated!
+  def self.reload # :nodoc:
+    self
   end
 end
