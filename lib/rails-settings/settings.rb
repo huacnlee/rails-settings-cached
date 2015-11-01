2ee0cd00a18ed210d362c36716fc8be489ab06c8
Alexander Adam <mail@alexander-adam.info>
2015-11-01 01:26:09 +0100

    include defaults in get_all call (PR #56)

--- lib/rails-settings/settings.rb
+++ lib/rails-settings/settings.rb
@@ -60,6 +60,8 @@ module RailsSettings
         vars.each do |record|
           result[record.var] = record.value
         end
+        vars.merge! @@defaults.slice(*(@@defaults.keys - result.keys))
+
         result.with_indifferent_access
       end
 
