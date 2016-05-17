1124650896188627d051139e5483628fd0ab1275
Stanislav Schultz <i@ssch.co>
2016-05-17 13:55:08 +0400

    Fix deprecated syntax in the model generator

--- lib/generators/settings/templates/model.rb
+++ lib/generators/settings/templates/model.rb
@@ -1,5 +1,5 @@
 # RailsSettings Model
-class <%= class_name %> < RailsSettings::CachedSettings
+class <%= class_name %> < RailsSettings::Base
   source Rails.root.join("config/app.yml")
   namespace Rails.env
 end
